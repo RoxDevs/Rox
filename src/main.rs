1c399cb931316fc462ed1560d85e427a1298ae28
muppi090909 <whthownothing@gmail.com>
2022-09-12 21:37:10 +0530

    merged

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,14 @@
+<<<<<<< HEAD
 use clap::Parser;
+mod parser;
 
 #[derive(Parser, Debug)]
 #[clap(about, version, author)]
 struct Values {
    #[clap(short = 'o', long)]
    desiredpackage: String,
+
+>>>>>>> d2e4ac9 (stash)
 }
 
 fn main() {
