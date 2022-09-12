c3870b1ed31ecd6b1e133e1dab910712b114bed8
unknown <kmalalamry8@gmail.com>
2022-09-12 09:26:21 +0300

    isgud

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, worffffld!");
+use clap::parser;
+#{derive(parser,debug)}
+#{clap(RK33DV&muppi090909,0.1,package manager written in rust,long_about = none)}
+
+struct Args {
+	
+	#{clap(short, long, value_praser, deafault_value_t = 1)}
+	
+	
 }
+
+//fn main() {
+    //println!("Hello, worffffld!");
+//}
