80f6567354b4f6966a08cf950e11eb79bb5a6bb3
RK33DV <kmalalamry8@gmail.com>
2022-09-18 21:17:54 +0300

    added some base code to upload files

--- src/Litespeed/mod.rs
+++ src/Litespeed/mod.rs
@@ -1,9 +1,21 @@
-use rusqlite::{Connection, Result};
+use rusqlite::{params, Connection, Result};
 use rusqlite::NO_PARAMS;
+struct package {
+    tarball: String,
+    win: i64,
+    lin: i64,
+    mac: i64,
+    major: String,
+    minor: String,
+    rev: String
+}
 
 mod addPackage{
     fn addPackage() {
-        
+        conn.execute(
+            "INSERT INTO package (tarball, win, lin, mac, major, minor,rev) VALUES (?1,?2,?3,?4,5?,6?,7?)",
+            params!(),
+        )?;
     }
     fn removePackage() {
         
