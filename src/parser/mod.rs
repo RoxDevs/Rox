eb8919fb246a6c59a90bd829688a510b65f90cc0
muppi090909 <whthownothing@gmail.com>
2022-09-18 09:54:47 +0530

    merged with rkdev

--- src/parser/mod.rs
+++ src/parser/mod.rs
@@ -4,24 +4,25 @@ use toml::from_str;
 // This is until the CLI is implemented
 #[allow(dead_code)]
 #[derive(Clone, PartialEq, Eq, PartialOrd, Ord, Debug, Deserialize)]
-pub struct Project {
-    tarball: String,
+pub struct RawProject {
     dependencies: Vec<String>,
-    mac: bool,
-    win: bool,
-    linux: bool,
+    mac: Option<String>,
+    win: Option<String>,
+    linux: Option<String>,
     major: String,
     minor: String,
     rev: String,
 }
 
-impl Project {
+// Until the CLI is built
+#[allow(dead_code)]
+impl RawProject {
     pub fn create_from_file(name: &str) -> Result<Self, Box<dyn std::error::Error>> {
         let file = std::fs::read_to_string(name)?;
-        Ok(Project::create_from_str(file.as_str())?.clone())
+        Ok(RawProject::create_from_str(file.as_str())?.clone())
     }
-    fn create_from_str(contents: &str) -> Result<Project, Box<dyn std::error::Error>> {
-        Ok(from_str::<Project>(contents)?.clone())
+    fn create_from_str(contents: &str) -> Result<RawProject, Box<dyn std::error::Error>> {
+        Ok(from_str::<RawProject>(contents)?.clone())
     }
 }
 
@@ -32,15 +33,14 @@ mod tests {
     #[test]
     fn from_str_tests() {
         let toml = include_str!("../../tomls/basic.toml");
-        let project = Project::create_from_str(toml).unwrap();
+        let project = RawProject::create_from_str(toml).unwrap();
         assert_eq!(
             project,
-            Project {
-                tarball: "www".to_string(),
+            RawProject {
                 dependencies: vec![],
-                mac: true,
-                win: false,
-                linux: true,
+                mac: Some("mac".to_string()),
+                win: None,
+                linux: Some("linux".to_string()),
                 major: "0".to_string(),
                 minor: "1".to_string(),
                 rev: "0".to_string()
