f70a45d1f4a09460427d53363d47b4cf9dc4cb85
muppi090909 <whthownothing@gmail.com>
2022-09-21 17:52:07 +0530

    merged main

--- src/parser/mod.rs
+++ src/parser/mod.rs
@@ -10,6 +10,7 @@ pub struct RawVer {
     win: Option<String>,
     linux: Option<String>,
     version: String,
+    git: Option<String>,
 }
 
 // Until the CLI is built
@@ -45,12 +46,18 @@ impl Into<Ver> for RawVer {
             major: version[0],
             minor: version[1],
             rev: version[2],
+            git: self.git,
         }
     }
 }
 
+<<<<<<< HEAD
 #[derive(Clone, PartialEq, Eq, PartialOrd, Ord, Debug)]
 struct Ver {
+=======
+#[derive(Clone, PartialEq, Eq, PartialOrd, Ord, Debug, Deserialize)]
+struct Project {
+>>>>>>> 174e939 (implemented source parsing)
     dependencies: Vec<Vec<String>>,
     mac: Option<String>,
     win: Option<String>,
@@ -58,6 +65,7 @@ struct Ver {
     major: usize,
     minor: usize,
     rev: usize,
+    git: Option<String>,
 }
 
 #[derive(Clone, PartialEq, Eq, PartialOrd, Ord, Debug, Deserialize)]
@@ -121,6 +129,10 @@ mod tests {
                 win: None,
                 linux: Some("linux".to_string()),
                 version: "0.1.0".to_string(),
+<<<<<<< HEAD
+=======
+                git: None
+>>>>>>> 174e939 (implemented source parsing)
             }
         )
     }
@@ -140,6 +152,29 @@ mod tests {
                 major: 0,
                 minor: 1,
                 rev: 0,
+<<<<<<< HEAD
+=======
+                git: None
+            }
+        )
+    }
+
+    #[test]
+    fn source_tests() {
+        let toml = include_str!("../../tomls/source.toml");
+        let project: Project = RawProject::create_from_str(toml).unwrap().into();
+        assert_eq!(
+            project,
+            Project {
+                dependencies: vec![],
+                mac: None,
+                linux: None,
+                major: 0,
+                minor: 1,
+                rev: 0,
+                git: Some("xxx/yyy".to_string()),
+                win: None
+>>>>>>> 174e939 (implemented source parsing)
             }
         )
     }
