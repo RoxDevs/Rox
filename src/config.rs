af47f33dba1ecc963343caf513485d082df2741c
muppi090909 <whthownothing@gmail.com>
2022-09-30 21:27:02 +0530

    added RawConfig

--- src/config.rs
+++ src/config.rs
@@ -1,14 +1,30 @@
 use serde::Deserialize;
+use std::{path::PathBuf, str::FromStr};
 
 #[derive(Debug, Deserialize, PartialEq)]
-pub struct Config {
-    pub path: String,
-    pub target: String,
+struct RawConfig {
+    path: String,
+    target: String,
 }
 
-impl Config {
+impl RawConfig {
     pub fn from_str(config: &str) -> Result<Self, toml::de::Error> {
-        toml::from_str(config)
+        toml::from_str::<RawConfig>(config)
+    }
+}
+
+#[derive(Debug, PartialEq)]
+struct Config {
+    path: PathBuf,
+    target: String,
+}
+
+impl Into<Config> for RawConfig {
+    fn into(self) -> Config {
+        Config {
+            path: PathBuf::from_str(self.path.as_str()).unwrap(),
+            target: self.target,
+        }
     }
 }
 
@@ -18,11 +34,11 @@ mod tests {
 
     #[test]
     fn basic_test() {
-        let config = Config::from_str(include_str!("../configs/example.toml")).unwrap();
+        let config: Config = RawConfig::from_str(include_str!("../configs/example.toml")).unwrap().into();
         assert_eq!(
             config,
             Config {
-                path: "/home/muppi/.rox".to_string(),
+                path: PathBuf::from_str("/home/muppi/.rox/").unwrap(),
                 target: "x86_64-unknown-linux-gnu".to_string()
             }
         )
